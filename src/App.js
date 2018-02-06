8f05af5e712cf1b5dabe84bb45088d2579c90e06
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-06 23:51:49 +0100

    very important point about how to update the states using copies and not the inmutable original state

--- src/App.js
+++ src/App.js
@@ -13,14 +13,29 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({ 
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // const person = this.state.persons[personIndex]; // this is not desirable since we are pointing to the original state
+    
+    // another approach is
+    // const person = Object.assign({},this.state.persons[personIndex]); // this is another good alternative
+    
+    // this is the most modern approach to copy the object is using the spread operator "..."
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value; // here we update the name in de person copy, not the original state wich is good!
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+    
+    this.setState({ persons: persons });
   }
 
   deletePersonHandler = (personIndex) => {
@@ -58,7 +73,8 @@ class App extends Component {
               click={this.deletePersonHandler.bind(this, index)}
               name={person.name}
               age={person.age} 
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
