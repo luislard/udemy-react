963dddd01d93f3fc151d4c7c3fc7e53f22c26a7f
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-06 23:12:27 +0100

    another approach to toggle persons

--- src/App.js
+++ src/App.js
@@ -50,6 +50,28 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Luis!')}
+            changed={this.nameChangedHandler}
+          >My Hobbies: Racing
+          </Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, This is a test</h1>
@@ -57,26 +79,7 @@ class App extends Component {
         <button 
           style={style}
           onClick={this.tooglePersonsHandler}>Switch Name</button>
-
-        {
-          this.state.showPersons ?
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Luis!')}
-                changed={this.nameChangedHandler}
-              >My Hobbies: Racing
-              </Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age} />
-            </div> : null
-        }
-        
+        {persons}
       </div>
     );
     // return React.createElement('div', null, 'h1', 'Hi, I am a React Luis!!!');
