3503e75f9b7f5d4ef11b087207f6f914aba995e9
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-08 23:23:14 +0100

    creating the cockpit component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import classes from './App.css';
 
+import classes from './App.css';
 import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -57,40 +58,19 @@ class App extends Component {
     // prefered way to toggle persons
     let persons = null;
 
-    let btnClass = '';
-
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          <Persons 
+      persons = <Persons 
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
-            changed={this.nameChangedHandler}
-            />
-        </div>
-      );
-
-      btnClass = classes.Red; // notice that this class Red is in the App.css pointing to an specific element but can be accesed in the global scope
-      
+            changed={this.nameChangedHandler} />;
     }
-
-    let assignedClasses = [];
-
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); 
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); 
-    }
-    
 
     return (
       <div className={classes.App}>
-        <h1>Hi, This is a test</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button 
-          className={btnClass}
-          onClick={this.tooglePersonsHandler}>Switch Name</button>
+      <Cockpit 
+        showPersons={this.state.showPersons} 
+        persons={this.state.persons}
+        clicked={this.tooglePersonsHandler} />
         {persons}
       </div>
     );
